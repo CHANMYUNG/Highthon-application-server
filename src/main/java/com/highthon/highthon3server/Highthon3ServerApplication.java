88442e4bc76c3e2a6974996361487c6a9054661a
Terry Yun <nooheat1228@gmail.com>
2018-04-07 23:44:17 +0900

    initialize project

---
+++ src/main/java/com/highthon/highthon3server/Highthon3ServerApplication.java
@@ -0,0 +1,12 @@
+package com.highthon.highthon3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Highthon3ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Highthon3ServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/highthon/highthon3server/Highthon3ServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.highthon.highthon3server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Highthon3ServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
