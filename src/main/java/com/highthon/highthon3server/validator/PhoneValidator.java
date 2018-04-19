d2a479c28155ff8f074586d9d95952ebf0bae467
Terry Yun <nooheat1228@gmail.com>
2018-04-19 23:07:21 +0900

    @Phone 정규표현식 바꿈 - 3자리-3,4자리-4자리 형식으로 교체

--- src/main/java/com/highthon/highthon3server/validator/PhoneValidator.java
+++ src/main/java/com/highthon/highthon3server/validator/PhoneValidator.java
@@ -8,6 +8,6 @@ import java.util.regex.Pattern;
 public class PhoneValidator implements ConstraintValidator<Phone, Object> {
     @Override
     public boolean isValid(Object value, ConstraintValidatorContext context) {
-        return Pattern.matches("(\\d{3}).*(\\d{3}).*(\\d{4})", value.toString());
+        return Pattern.matches("^\\d{3}-\\d{3,4}-\\d{4}$", value.toString());
     }
 }
