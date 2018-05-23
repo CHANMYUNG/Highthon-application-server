b87bcf3473aa0b5df830e9fec68864b3dd5425c3
Terry Yun <nooheat1228@gmail.com>
2018-05-23 23:54:48 +0900

    swagger documentation enabled

--- src/main/java/com/highthon/highthon3server/controller/AdminController.java
+++ src/main/java/com/highthon/highthon3server/controller/AdminController.java
@@ -3,6 +3,9 @@ package com.highthon.highthon3server.controller;
 import com.highthon.highthon3server.exception.SelfHandleException;
 import com.highthon.highthon3server.security.JwtTokenUtil;
 import com.highthon.highthon3server.service.admin.AdminService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +25,11 @@ public class AdminController {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
+
+    @ApiOperation("관리자 삭제")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "adminId", value = "관리자 고유 아이디", required = true, dataType = "string", paramType = "path")
+    })
     @DeleteMapping("/admin/{adminId}")
     public void deleteAdmin(@PathVariable("adminId") String adminId, HttpServletRequest request) {
         String token = request.getHeader(tokenHeader).substring(7);
--- src/main/java/com/highthon/highthon3server/controller/ApplicationController.java
+++ src/main/java/com/highthon/highthon3server/controller/ApplicationController.java
@@ -7,12 +7,16 @@ import com.highthon.highthon3server.dto.application.ApplicationSaveDto;
 import com.highthon.highthon3server.dto.application.GetApplicationConditionDto;
 import com.highthon.highthon3server.dto.application.SaveResponse;
 import com.highthon.highthon3server.service.application.ApplicationService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -24,26 +28,41 @@ public class ApplicationController {
     @Autowired
     private ApplicationService applicationService;
 
+    @ApiOperation("하이톤 참가 신청")
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "area", value = "참가 분야 (생활/게임)", required = true, dataType = "string", allowableValues = "LIFE, GAME", paramType = "body", example = "LIFE"),
+//            @ApiImplicitParam(name = "belong", value = "소속(학교) -등학교 형식", required = true, dataType = "string", paramType = "body", example = "대덕소프트웨어마이스터고등학교"),
+//            @ApiImplicitParam(name = "email", value = "이메일", required = true, dataType = "string", paramType = "body", example = "nooheat1228@gmail.com"),
+//            @ApiImplicitParam(name = "sex", value = "성별", required = true, dataType = "string", paramType = "body", allowableValues = "MAN, WOMAN", example = "MAN"),
+//            @ApiImplicitParam(name = "phone", value = "전화번호 000-0000-0000 형식", required = true, dataType = "string", paramType = "body", example = "000-0000-0000"),
+//            @ApiImplicitParam(name = "position", value = "포지션 (개발자/디자이너)", required = true, dataType = "string", paramType = "body", allowableValues = "DEVELOP, DESIGN", example = "DEVELOP"),
+//            @ApiImplicitParam(name = "name", value = "이름", required = true, dataType = "string", paramType = "body", example = "윤찬명"),
+//            @ApiImplicitParam(name = "password", value = "비밀번호", required = true, dataType = "string", paramType = "body", example = "thisispassword")
+//    })
     @PostMapping("/apply")
-    public ResponseEntity<SaveResponse> apply(@Valid @RequestBody ApplicationSaveDto dto) {
+    public ResponseEntity<SaveResponse> apply(/*@ApiIgnore*/ @Valid @RequestBody ApplicationSaveDto dto) {
         return new ResponseEntity<SaveResponse>(applicationService.saveApplication(dto), HttpStatus.CREATED);
     }
 
+    @ApiOperation("하이톤 참가 신청 상태 조회")
     @PostMapping("/apply/status")
     public ResponseEntity<ApplicationCondition> getApplicationCondition(@Valid @RequestBody GetApplicationConditionDto dto) {
         return new ResponseEntity<ApplicationCondition>(applicationService.getApplicationCondition(dto), HttpStatus.OK);
     }
 
+    @ApiOperation("접수된 하이톤 참가 신청 목록 조회")
     @GetMapping("/applications/accepted")
     public List<Application> getAcceptedApplications(Pageable pageable) {
         return applicationService.getAcceptedApplications(pageable);
     }
 
+    @ApiOperation("대기중 하이톤 참가 신청 목록 조회")
     @GetMapping("/applications/waiting")
     public List<ApplicationIncludesWaitingNumber> getWaitingApplications(Pageable pageable) {
         return applicationService.getWaitingApplications(pageable);
     }
 
+    @ApiOperation("하이톤 참가 신청 삭제")
     @DeleteMapping("/applications/{applicationId}")
     public void deleteApplication(@PathVariable("applicationId") Long applicationId) {
         applicationService.deleteApplicationAndUpdateLatestWaitingApplicationToAccepted(applicationId);
