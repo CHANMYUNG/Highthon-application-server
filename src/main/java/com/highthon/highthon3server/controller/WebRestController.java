49106e1167b5dba2d8a8d8acc9a3bcf0e0de6f28
Terry Yun <nooheat1228@gmail.com>
2018-05-23 13:33:18 +0900

    use codeDeploy & NginX nonstop deploy

---
+++ src/main/java/com/highthon/highthon3server/controller/WebRestController.java
@@ -0,0 +1,34 @@
+package com.highthon.highthon3server.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private Environment env;
+
+    @GetMapping("/health")
+    public Map<String, String> getHealth() {
+        Map<String, String> map = new HashMap<>();
+        map.put("status", "UP");
+        return map;
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        Set<String> profileSet = Arrays.stream(env.getActiveProfiles()).collect(Collectors.toSet());
+        if (profileSet.contains("set2")) return "set2";
+        else return "set1";
+    }
+}
